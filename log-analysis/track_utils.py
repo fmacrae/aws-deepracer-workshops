87d3b37c455a17e5b730b63f177f852f68da9405
Finlay Macrae <finlay.macrae@gmail.com>
2019-10-20 19:20:16 +0000

    Fixed bug where tracks showed few corners

--- log-analysis/track_utils.py
+++ log-analysis/track_utils.py
@@ -1,15 +1,24 @@
 import numpy as np
+import matplotlib as mpl
 
 # Shapely Library
 from shapely.geometry import Polygon
 from shapely.geometry.polygon import LineString
+from typing import Tuple
 
 
-def get_track_waypoints(track_name, absolute_path="."):
+
+
+def get_track_waypoints(track_name: str, absolute_path=".") -> np.array:
     return np.load("%s/tracks/%s.npy" % (absolute_path, track_name))
+get_track_waypoints.__doc__ = """Returns a numpy array containing the waypoints for 
+the inner, mid and outer track boundaries way points 
+as a numpy array with the format 
+[0:2]=center, [2:4]=inner and [4:6]=outer.  
+First and last index are duplicates.  Use 
+load_track as it splits the waypoints better."""
 
-
-def load_track(track_name, absolute_path="."):
+def load_track(track_name: str, absolute_path=".") -> Tuple[np.array, np.array, np.array, Polygon]:
     if track_name.endswith('.npy'):
         track_name = track_name[:-4]
 
@@ -27,23 +36,33 @@ def load_track(track_name, absolute_path="."):
     outer_border = waypoints[:, 4:6] * 100
 
     return center_line, inner_border, outer_border, road_poly
+load_track.__doc__ = """Returns 3 numpy arrays containing the coordinate tuples 
+for the center, inner, outer and outer track boundaries 
+way points and a Polygon containing the full road layout.  
+Coordinates are scaled as centemeters.  
+First and last index are duplicates."""
 
-
-def get_angle(p0, p1, p2):
+def get_angle(p0: np.array, p1: np.array, p2: np.array) -> np.degrees:
     v0 = np.array(p0) - np.array(p1)
     v1 = np.array(p2) - np.array(p1)
 
     angle = np.math.atan2(np.linalg.det([v0, v1]), np.dot(v0, v1))
     return np.degrees(angle)
+get_angle.__doc__ = """Takes three points x,y coordinates as input and returns the angle 
+                       between at p1.  180 or -180 is a straight line."""
 
 
-def get_vector_length(v):
+def get_vector_length(v: np.array) -> float:
     return np.sqrt(v[0] ** 2 + v[1] ** 2)
+get_vector_length.__doc__ = """Returns the length of the Hypotenuse
+for a vector passed in as a two row array
+"""
 
-
-def normalize_vector(v):
+def normalize_vector(v:np.array) -> np.array:
     return v / get_vector_length(v)
-
+normalize_vector.__doc__ = """Convert vector to length of 1
+for a vector passed in as a two row array
+"""
 
 # TODO Watch this, it might be putting points inside out
 def get_orthogonal_vector_for_straight_line(before, point):
@@ -95,7 +114,7 @@ def is_point_on_the_line(l1_x1, l1_y1, l1_x2, l1_y2, x1, x2):
     return a1 < 5 and a2 < 5
 
 
-def plot_trackpoints(trackpoints, show=True):
+def plot_trackpoints(trackpoints: np.array, show=True):
     import matplotlib.pyplot as plt
     for point in trackpoints:
         plt.scatter(point[0], point[1], c="blue")
@@ -103,3 +122,9 @@ def plot_trackpoints(trackpoints, show=True):
         plt.scatter(point[4], point[5], c="cyan")
     if show:
         plt.show()
+    return
+plot_trackpoints.__doc__ = """Takes in a numpy array that in the format provided by
+get_track_waypoints and displays them as a scatter chart 
+with this colour code - blue - center
+black inner
+cyan outer"""
